71cc9b824a3babb7553b17615f94ac4a3cad5585
Arham khan <khanarham8962@gmail.com>
2024-08-27 15:37:04 +0530

    Delete DapperMvcDemo.Data directory

--- DapperMvcDemo.Data/DataAccess/ISqlDataAccess.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DapperMvcDemo.Data.DataAccess
-{
-    public interface ISqlDataAccess
-    {
-        Task<IEnumerable<T>> GetData<T, P>(string spName, P parameters, string connectionId = "conn");
-        Task SaveData<T>(string spName, T parameters, string connectionId = "conn");
-    }
-}
